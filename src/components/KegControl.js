2fd54acd3dfc8754b51c169e2b1a52fd9fe5d290
Jason M <Jbird206@gmail.com>
2020-05-24 13:53:15 -0700

    added reducers to control and ActionTypes

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -12,92 +12,78 @@ class KegControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
-      masterKegList: [
-        {
-          name: "Schlutz",
-          brand: "Frat Guyz",
-          flavor: " Cheap, crisp and guzzleable",
-          price: "42",
-          alcohol: "6.20",
-          pints: 100,
-          id: 0
-        },
-        {
-          name: "Citrus Crush",
-          brand: "Mountain Bros.",
-          flavor: " A citrusy IPA with stong hop notes",
-          price: "85",
-          alcohol: "7.2",
-          pints: 100,
-          id: 1
-        },
-        {
-          name: "Irish Breath",
-          brand: "Brim Reaper",
-          flavor: " A dark, nutty and strong ale",
-          price: "59",
-          alcohol: "11.5",
-          pints: 100,
-          id: 2
-        }
-      ],
-      selectedKeg: null
+      selectedKeg: null,
+      editing:false
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick = () => {
-    if (this.state.selectedKeg != null) {
-      this.setState({
-        formVisibleOnPage: false,
-        selectedKeg: null
-      });
-    } else {
-      this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage,
-      }));
-    }
+    const { dispatch } = this.props;
+    const action = a.toggleForm();
+    dispatch(action);
+    this.setState({selectedKeg: null});
   }
 
   handleAddingNewKegToList = (newKeg) => {
-    const newMasterKegList = this.state.masterKegList.concat(newKeg);
-    this.setState({
-      masterKegList: newMasterKegList,
-      formVisibleOnPage: false 
-    });
+    const { dispatch } = this.props;
+    const action = a.addKeg(newKeg);
+    dispatch(action);
+    const action2 = a.toggleForm();
+    dispatch(action2);
   }
 
   handleChangingSelectedKeg = (id) => {
-    //const selectedKeg = this.state.masterKegList.filter(keg => keg.id === id)[0];
     const selectedKeg = this.props.masterKegList[id];
     this.setState({selectedKeg: selectedKeg});
   }
 
-  handleBuyingPint = (name) => {
-    const keg = this.state.masterKegList.filter(keg => keg.name === name)[0];
-    if(keg.pints > 0){
-      keg.pints = keg.pints - 1;
-    }
-    const otherKegsList = this.state.masterKegList.filter(keg => keg.name !== name);
-    const newMasterKegList = [...otherKegsList, keg];
-    this.setState({
-      ...this.state,
-      masterKegList: newMasterKegList,
-      
-    })
+  handleBuyingPint = (keg) => {
+    const { dispatch } = this.props;
+    const action = a.buyPint(keg);
+    dispatch(action);
   }
-
-
+  //       {
+  //         name: "Schlutz",
+  //         brand: "Frat Guyz",
+  //         flavor: " Cheap, crisp and guzzleable",
+  //         price: "42",
+  //         alcohol: "6.20",
+  //         pints: 100,
+  //         id: 0
+  //       },
+  //       {
+  //         name: "Citrus Crush",
+  //         brand: "Mountain Bros.",
+  //         flavor: " A citrusy IPA with stong hop notes",
+  //         price: "85",
+  //         alcohol: "7.2",
+  //         pints: 100,
+  //         id: 1
+  //       },
+  //       {
+  //         name: "Irish Breath",
+  //         brand: "Brim Reaper",
+  //         flavor: " A dark, nutty and strong ale",
+  //         price: "59",
+  //         alcohol: "11.5",
+  //         pints: 100,
+  //         id: 2
+  //       }
+  //     ],
+  //     selectedKeg: null
+  //   };
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
 
   render(){
     let currentlyVisibleState = null;
     let buttonText = null;
 
     if (this.state.selectedKeg != null) {
-      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} />
+      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} onBuyingPint={this.handleBuyingPint}/>
       buttonText = "Return to Keg List";
-    } else if (this.state.formVisibleOnPage) {
+    } else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />;
       buttonText = "Return to Keg List";
     } else {
@@ -112,7 +98,7 @@ class KegControl extends React.Component {
             <button onClick={this.handleClick}>{buttonText}</button>
           </div>
           <div id="DailyKeg">
-          <DailyKeg kegList={this.props.masterKegList} onKegSelection={this.handleChangingSelectedKeg} />
+            <DailyKeg kegList={this.props.masterKegList} onKegSelection={this.handleChangingSelectedKeg} />
           </div>
         </div>
       </React.Fragment>
@@ -127,7 +113,8 @@ KegControl.propTypes = {
 
 const mapStateToProps = state => {
   return {
-    masterKegList: state
+    masterKegList: state.masterKegList,
+    formVisibleOnPage: state.formVisibleOnPage
   }
 }
 
